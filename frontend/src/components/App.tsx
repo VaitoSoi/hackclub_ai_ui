be11b97705afb0dac406d064db95bdae1b510009
VaitoSoi <duyduy15456@gmail.com>
2025-08-21 00:09:45 +0700

    add router

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,12 +1,27 @@
+import { useState, type ComponentProps } from "react";
 import "../index.css";
-import Chat from "./Chat/Chat";
-import Header from "./Chat/Header";
+import { Toaster } from "./ui/sonner";
+import Sidebar from "./Sidebar/Sidebar";
+import { SidebarProvider } from "./ui/sidebar";
 
-export default function () {
-    return <div className="w-screen h-screen flex bg-[#0c263d]">
-        <div className="m-10 w-full flex flex-col gap-4">
-            <Header />
-            <Chat />
-        </div>
-    </div>;
+export default function ({ children }: ComponentProps<"div">) {
+    const [open, setOpen] = useState<boolean>(true);
+
+    return <>
+        <SidebarProvider
+            style={{
+                "--sidebar-width": "20rem"
+            } as React.CSSProperties}
+            open={open}
+            onOpenChange={setOpen}
+        >
+            <Toaster />
+            <Sidebar />
+            <div className="w-screen h-screen flex bg-[#171717]">
+                <div className={"m-5 w-full flex flex-col " + (open && "ml-0")}>
+                    {children}
+                </div>
+            </div>
+        </SidebarProvider>
+    </>;
 }
